f4dda3ff290b9ba1cd8adf221ac0b7fa681200cb
gqwilcox <78162279+gqwilcox@users.noreply.github.com>
2024-05-11 04:33:49 -0700

    Update AbstractDBAdapter.java

--- lab4-questions/src/main/java/edu/canisius/cyb600/lab4/database/AbstractDBAdapter.java
+++ lab4-questions/src/main/java/edu/canisius/cyb600/lab4/database/AbstractDBAdapter.java
@@ -1,6 +1,11 @@
 package edu.canisius.cyb600.lab4.database;
 
+import edu.canisius.cyb600.lab4.dataobjects.Actor;
+import edu.canisius.cyb600.lab4.dataobjects.Category;
+import edu.canisius.cyb600.lab4.dataobjects.Film;
+
 import java.sql.Connection;
+import java.util.List;
 
 /**
  * Abstract DB Adapter
@@ -11,6 +16,16 @@ public abstract class AbstractDBAdapter {
     public AbstractDBAdapter(Connection conn) {
         this.conn = conn;
     }
+
+    public abstract List<String> getCategories();
+
+    public abstract List<Film> checkFilmLength(int length);
+
+    public abstract List<Actor> checkFirstLetterFirstName(char firstLetter);
+
+    public abstract List<Film> getFilmCat(Category category);
+
+    public abstract List<Actor> insertActorsByLastName(List<Actor> actors);
 
     //SELECTS
 
